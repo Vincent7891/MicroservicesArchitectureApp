53cb59abf38009ac9f71362f20efc9464aeae579
Vincent <vincent.audibertdejean@gmail.com>
2024-11-12 21:16:08 +0000

    feat: setu python service and dockerfile

--- node-service/app.ts
+++ node-service/app.ts
@@ -6,3 +6,7 @@ const app = express()
 app.get("/", (req, res) => {
 	res.send(`Hello from Node.js service! Host: ${os.hostname()}`)
 })
+
+app.listen(8080, () => {
+	console.log("Node.js service listening on port 8080")
+})
